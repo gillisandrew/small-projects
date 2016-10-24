6694badf94bd588722598735cf0a2c65eb6d0cc4
Andrew Gillis <Andrew Gillis>
2016-10-24 14:45:43 -0400

    CORS?

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 var smallprojects = (function() {
     var nav = "#projects"
-    var nav_item = '<li role="presentation"><a href="#"></a></li>';
-    $.getJSON('projects.json', function(json) {
-        var item = $.parseHTML(nav_item);
-        $.each(json, function(json, item) {
-            $(item).find('a').href(json.directory);
-            $(nav).append(nav_item)
-        });
+
+    var projects = [{
+        "title": "Server Information",
+        "directory": "server-information"
+    },
+    {
+        "title": "Simon Game",
+        "directory": "simon-game"
+    },
+    {
+        "title": "Random Quote Machine",
+        "directory": "random-quote"
+    }]
+
+    for(var i = 0; i<projects.length; i++) {
+        $.getJSON(projects[i].directory + '/index.html', function(html) {
+            projects[i]["HTML"] = html;
+        })
+        $.getJSON(projects[i].directory + '/index.js', function(js) {
+            projects[i]["JS"] = js;
+        })
+        $.getJSON(projects[i].directory + '/index.js', function(css) {
+            projects[i]["CSS"] = css;
+        })
+    }
+
+    $(nav + ' li a').on('click', function() {
+        $('#code pre').html(projects[$(this).data('project')][$('#code .active').data('content')])
     });
 
+$(document).ready(function() {
+    $.each(projects, function(i, project, item) {
+        var item = '<li role="presentation"><a data-project="' + i + '" href="#'+ project.directory +'">' + project.title + '</a></li>';
+        $(nav).append(item)
+    });
+});
+    
 })();
