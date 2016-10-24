30d28907264d7f3ed317cf06fabe853a3a7f91dc
Andrew Gillis <Andrew Gillis>
2016-10-24 15:04:15 -0400

    remove built-in json

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
 var smallprojects = (function() {
-    var nav = "#projects"
-
-    var projects = [{
-        "title": "Server Information",
-        "directory": "server-information"
-    },
-    {
-        "title": "Simon Game",
-        "directory": "simon-game"
-    },
-    {
-        "title": "Random Quote Machine",
-        "directory": "random-quote"
-    }]
+    var nav = "#projects";
 
     $.getJSON('projects.json', function(json) {
-        projects = json;
         for(var i = 0; i<json.length; i++) {
             var item = '<li role="presentation"><a data-project="' + i + '" href="#'+ json[i].directory +'">' + json[i].title + '</a></li>';
             $(nav).append(item)
             $.getJSON(projects[i].directory + '/index.html', function(html) {
-                projects[i]["HTML"] = html;
+                json[i]["HTML"] = html;
             })
             $.getJSON(projects[i].directory + '/index.js', function(js) {
-                projects[i]["JS"] = js;
+                json[i]["JS"] = js;
             })
             $.getJSON(projects[i].directory + '/index.js', function(css) {
-                projects[i]["CSS"] = css;
+                json[i]["CSS"] = css;
             })
         }
+        projects = json;
     });
-$(document).ready(function() {
-    $('#projects li a').on('click', function() {
-        console.log(projects);
-        $('#projects li.active').removeClass('active');
-        $(this).parent('li').addClass('active');
-        $('#code pre').html(projects[$(this).data('project')][$('#code .active').data('content')])
+    $(document).ready(function() {
+        $('#projects li a').on('click', function() {
+            console.log(projects);
+            $('#projects li.active').removeClass('active');
+            $(this).parent('li').addClass('active');
+            $('#code pre').html(projects[$(this).data('project')][$('#code .active').data('content')])
+        });
     });
-});
     
     
 })();
