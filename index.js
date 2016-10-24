29999d8c355a88539846be503b459d0b9bc62fe5
Andrew Gillis <Andrew Gillis>
2016-10-24 15:09:14 -0400

    fix async issues

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ var smallprojects = (function() {
     var nav = "#projects";
     var projects;
     $.getJSON('projects.json', function(json) {
+        projects = json;
         for(var i = 0; i<json.length; i++) {
-            var item = '<li role="presentation"><a data-project="' + i + '" href="#'+ json[i].directory +'">' + json[i].title + '</a></li>';
+            var item = '<li role="presentation"><a data-project="' + i + '" href="#'+ projects[i].directory +'">' + projects[i].title + '</a></li>';
             $(nav).append(item)
-            $.getJSON(json[i].directory + '/index.html', function(html) {
-                json[i]["HTML"] = html;
+            $.getJSON(projects[i].directory + '/index.html', function(html) {
+                console.log(content)
+                projects[i]["HTML"] = html;
             })
-            $.getJSON(json[i].directory + '/index.js', function(js) {
-                json[i]["JS"] = js;
+            $.getJSON(projects[i].directory + '/index.js', function(js) {
+                projects[i]["JS"] = js;
             })
-            $.getJSON(json[i].directory + '/index.js', function(css) {
-                json[i]["CSS"] = css;
+            $.getJSON(projects[i].directory + '/index.js', function(css) {
+                projects[i]["CSS"] = css;
             })
         }
-        projects = json;
         console.log(projects);
     });
     $(document).ready(function() {
