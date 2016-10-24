a6d221e3866b6883e191ad0c3a6021f44140b78d
Andrew Gillis <Andrew Gillis>
2016-10-24 14:26:03 -0400

    fix HTML parsing

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var smallprojects = (function() {
     var nav = "#projects"
     var nav_item = '<li role="presentation"><a href="#"></a></li>';
     $.getJSON('projects.json', function(json) {
-        $.each(json, function(json) {
-            $(nav_item).find('a').href(json.directory);
+        var item = $.parseHTML(nav_item);
+        $.each(json, function(json, item) {
+            $(item).find('a').href(json.directory);
             $(nav).append(nav_item)
         });
     });
