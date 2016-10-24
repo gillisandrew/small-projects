53d85eddf9af40d349506b49e312246ef70cd5c4
Andrew Gillis <Andrew Gillis>
2016-10-24 14:52:25 -0400

    fix o 0 mixup

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var smallprojects = (function() {
 
     $.getJSON('projects.json', function(json) {
         projects = json;
-        for(var i = o; i<json.length; i++) {
+        for(var i = 0; i<json.length; i++) {
             var item = '<li role="presentation"><a data-project="' + i + '" href="#'+ project.directory +'">' + project.title + '</a></li>';
             $(nav).append(item)
         }
