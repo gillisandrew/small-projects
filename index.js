d041981bdbdf5c92dd6ba8776f46000fb169aab9
Andrew Gillis <Andrew Gillis>
2016-10-24 15:15:58 -0400

    change naming

--- index.js
+++ index.js
@@ -16,18 +16,19 @@ var smallprojects = (function() {
     for(var i = 0; i<projects.length; i++) {
         var item = '<li role="presentation"><a data-project="' + i + '" href="#'+ projects[i].directory +'">' + projects[i].title + '</a></li>';
         $(nav).append(item)
+
         $.getJSON(projects[i].directory + '/index.html', function(html) {
-            console.log(content)
-            projects[i]["HTML"] = html;
+            projects[i].HTML = html;
         })
         $.getJSON(projects[i].directory + '/index.js', function(js) {
-            projects[i]["JS"] = js;
+            projects[i].JS = js;
         })
         $.getJSON(projects[i].directory + '/index.js', function(css) {
-            projects[i]["CSS"] = css;
-        })
+            projects[i].CSS = css;
+        });
     }
     $(document).ready(function() {
+        
         $('#projects a').on('click', function() {
             console.log(projects);
             // $('#projects li.active').removeClass('active');
