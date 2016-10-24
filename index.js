814da0f1665dcaa1f848113170476c3c42c7748d
Andrew Gillis <Andrew Gillis>
2016-10-24 15:13:57 -0400

    remove getJSON

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 var smallprojects = (function() {
     var nav = "#projects";
-    var projects;
-    $.getJSON('projects.json', function(json) {
-        projects = json;
-        for(var i = 0; i<json.length; i++) {
-            var item = '<li role="presentation"><a data-project="' + i + '" href="#'+ projects[i].directory +'">' + projects[i].title + '</a></li>';
-            $(nav).append(item)
-            $.getJSON(projects[i].directory + '/index.html', function(html) {
-                console.log(content)
-                projects[i]["HTML"] = html;
-            })
-            $.getJSON(projects[i].directory + '/index.js', function(js) {
-                projects[i]["JS"] = js;
-            })
-            $.getJSON(projects[i].directory + '/index.js', function(css) {
-                projects[i]["CSS"] = css;
-            })
-        }
-        console.log(projects);
-    });
+    var projects = [{
+        "title": "Server Information",
+        "directory": "server-information"
+    },
+    {
+        "title": "Simon Game",
+        "directory": "simon-game"
+    },
+    {
+        "title": "Random Quote Machine",
+        "directory": "random-quote"
+    }];
+
+    for(var i = 0; i<projects.length; i++) {
+        var item = '<li role="presentation"><a data-project="' + i + '" href="#'+ projects[i].directory +'">' + projects[i].title + '</a></li>';
+        $(nav).append(item)
+        $.getJSON(projects[i].directory + '/index.html', function(html) {
+            console.log(content)
+            projects[i]["HTML"] = html;
+        })
+        $.getJSON(projects[i].directory + '/index.js', function(js) {
+            projects[i]["JS"] = js;
+        })
+        $.getJSON(projects[i].directory + '/index.js', function(css) {
+            projects[i]["CSS"] = css;
+        })
+    }
     $(document).ready(function() {
         $('#projects a').on('click', function() {
             console.log(projects);
